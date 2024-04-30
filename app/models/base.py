84d68448acb8d5dae189f22beeee3040b1581800
Iv-EN <engenivanov@yandex.ru>
2024-04-30 22:07:31 +0300

    changed create_date default

--- app/models/base.py
+++ app/models/base.py
@@ -17,5 +17,5 @@ class BaseModel(Base):
     full_amount = Column(Integer)
     invested_amount = Column(Integer, default=0)
     fully_invested = Column(Boolean, default=False)
-    create_date = Column(DateTime, server_default=func.now())
+    create_date = Column(DateTime, default=func.now())
     close_date = Column(DateTime)
